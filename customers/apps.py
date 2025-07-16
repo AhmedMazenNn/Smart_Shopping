dc6da9ac1ce8e9a046d034afb106d25bf8892653
Ahmed Mazen <ahmedmazenm@outlook.com>
2025-07-16 21:07:29 +0300

    added imported signals to the file

--- customers/apps.py
+++ customers/apps.py
@@ -9,10 +9,4 @@ class CustomersConfig(AppConfig):
     verbose_name = 'العملاء'
 
     def ready(self):
-        """
-        تُستدعى هذه الدالة عند بدء تشغيل تطبيق Django.
-        لا يوجد signals.py داخل Customers حاليًا، لذا لا حاجة لاستيراد إشارات هنا.
-        إذا أضفت إشارات لاحقًا، فقم باستيرادها هنا.
-        """
-        # import customers.signals # <--- قم بإلغاء التعليق إذا كان لديك signals.py في تطبيق customers
-        pass # <--- استخدم pass إذا لم يكن هناك أي شيء آخر تفعله في ready()
+        import customers.signals
